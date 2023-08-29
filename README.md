41c26a20cace65459e575fee2a45705f0f2a03ae
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2023-08-29 13:00:30 +0000

    feat: support ctfcli v0.0.14, templating, and connection info

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # ctfcli-deploy-kubernetes
+
 A [ctfcli](https://github.com/CTFd/ctfcli) plugin for deploying CTF challenge containers to Kubernetes.
 
 ## Installation
+
 1. Install the plugin: `ctf plugins install https://github.com/pl4nty/ctfcli-deploy-kubernetes`
 2. [Install Kompose](https://kompose.io/installation/)
 3. Install `kubectl` and configure cluster access, eg [Google Kubernetes Engine docs](https://cloud.google.com/kubernetes-engine/docs/how-to/cluster-access-for-kubectl)
 4. Login to a container registry, eg `docker login ghcr.io`
 
 ## Usage
+
 1. Add a [Compose](https://www.compose-spec.io/) file like `docker-compose.yml` to your challenge folder
 2. `ctf plugins deploy_k8s --registry ghcr.io/username --challenge challengefolder`
 
 ## Pwn challenges
+
 To expose pwn challenges to the internet:
 
 1. Ensure challenges have unique ports
@@ -21,29 +25,39 @@ To expose pwn challenges to the internet:
 5. Visit your challenge at `chals.example.com:1234`
 
 ## Web challenges
+
 To expose web challenges to the internet:
 
 1. [Install an ingress controller](https://kubernetes.io/docs/concepts/services-networking/ingress-controllers/)
-2. Set it as the default IngressClass: `kubectl annotate ingressclass your-ingress-class ingressclass.kubernetes.io/is-default-class=true`
+2. Set it as the default IngressClass: `kubectl annotate ingressclass your-ingress-class ingressclass.kubernetes.io/is-default-class=true`. Alternatively, set `kompose.service.expose.tls-secret` in a [template](#templating)
 3. Choose a domain for challenges to be exposed on subdomains, eg `chals.example.com`
 4. `ctf plugins deploy_k8s --registry ghcr.io/username --challenge challengefolder --domain chals.example.com`
 5. Configure DNS for `*.chals.example.com` to point to your ingress controller external IP address
 6. Visit your challenge at `http://challenge-name.chals.example.com`
-7. (Optional) Configure TLS termination at the ingress controller. If using the Ingress TLS field, create a Kubernetes secret in the challenge namespace named `listener-tls-secret` or use `--ingressTlsSecret`. This can be automated with [reflector](https://github.com/emberstack/kubernetes-reflector) for wildcard certificates (recommended) or [cert-manager](https://cert-manager.io/).
-
-`--ingressClassName` is available to support non-default ingress classes.
+7. (Optional) Configure TLS termination at the ingress controller. If using the Ingress TLS field, create a Kubernetes secret in the challenge namespace and set `kompose.service.expose.tls-secret` in a [template](#templating). Secret creation can be automated with [reflector](https://github.com/emberstack/kubernetes-reflector) for wildcard certificates (recommended) or [cert-manager](https://cert-manager.io/).
 
 ## Private container registries
-To use a private container registry with password authentication, add `--registrySecret secret-name` and create a Kubernetes secret in the challenge namespace. 
 
-This can be done automatically:
+To use a private container registry with password authentication, create a Kubernetes secret in the challenge namespace. Secret creation can be automated with [reflector](https://github.com/emberstack/kubernetes-reflector).
 
 ```sh
-output=$(ctf plugins deploy_k8s ghcr.io/username chals.example.com)
-echo $output
-namespace=$(echo $output | rev | cut -d " " -f1 | rev)
 kubectl create secret docker-registry ghcr --namespace=$namespace \
 --docker-server=ghcr.io \
 --docker-username=username \
 --docker-password=password
+```
+
+Then set `kompose.image-pull-secret` in a [template](#templating).
+
+## Templating
+
+Use `--template` with a [Compose](https://www.compose-spec.io/) file to merge it into challenges. This can enable certain features. For a full list of supported `kompose` labels, see the labels section of the [Kompose docs](https://kompose.io/user-guide/).
+
+```yaml
+services:
+  app:
+    labels:
+      kompose.image-pull-secret: 'mypullsecretname'
+      kompose.service.expose.ingress-class-name: 'myingressclass'
+      kompose.service.expose.tls-secret: 'mytlssecretname'
 ```
--- __init__.py
+++ __init__.py
@@ -1,139 +1,149 @@
-from types import MethodType
-from ctfcli.utils.challenge import (
-	load_challenge,
-)
-from ctfcli.utils.config import (
-    load_config,
-)
-
+import logging
 import os
-from pathlib import Path
-import re
 import subprocess
 import tempfile
+import re
+from pathlib import Path
+from types import MethodType
 
 import click
-import yaml
+import hiyapyco
 from slugify import slugify
 
+from ctfcli.core.challenge import Challenge
+from ctfcli.core.config import Config
+from ctfcli.core.exceptions import ChallengeException
+
+log = logging.getLogger("ctfcli.cli.challenges")
+
 # TODO replace ingress and annotations with strategic merge patch
-def deploy_kubernetes(self, registry, registrySecret=None, challenge=None, domain=None,
-	ingressClassName=None, ingressTlsSecret=None, annotations=None):
-	# boilerplate from https://github.com/CTFd/ctfcli/blob/7b4a09af8414eb1f5f6da9a8422fb53b5e9cbc15/ctfcli/cli/challenges.py#L301
+def deploy_kubernetes(
+	self,
+	registry: str,
+	challenge: str = None,
+	domain: str = None,
+	template: str = None,
+	registrySecret=None, ingressTlsSecret=None
+) -> str:
+	# boilerplate from https://github.com/CTFd/ctfcli/blob/c385e70c5553dea013b1b6c99fbe353324e5f3fe/ctfcli/cli/challenges.py#L621
+	log.debug(f"deploy_k8s: (registry={registry}, challenge={challenge}, domain={domain}, template={template})")
+	
+	config = Config()
+	challenge_keys = [challenge]
+
 	if challenge is None:
-		challenge = os.getcwd()
+		challenge_keys = config.challenges.keys()
 
-	path = Path(challenge)
+	failed_deployments = []
 
-	if path.name.endswith(".yml") is False:
-		path = path / "challenge.yml"
+	deployable_challenges = []
+	for challenge_key in challenge_keys:
+		challenge_path = config.project_path / Path(challenge_key)
 
-	click.secho(f"Found {path}")
-	challenge = load_challenge(path)
-	click.secho(f'Loaded {challenge["name"]}', fg="yellow")
+		if not challenge_path.name.endswith(".yml"):
+			challenge_path = challenge_path / "challenge.yml"
 
-	# autodetect category for convenience, unlike ctfcli
-	category = getattr(challenge, "category", str(challenge.directory.parent))
-	namespace = slugify(f"{category}-{challenge['name']}")
+		try:
+			challenge = Challenge(challenge_path)
 
-	# https://github.com/kubernetes/kompose/blob/17fbe3b4632cca0f61a7fda5f3b91034cdf5c5e4/pkg/app/app.go#L35-L43
-	defaultComposeFiles = ["compose.yaml", "compose.yml", "docker-compose.yaml", "docker-compose.yml"]
-	composeFiles = [p for p in [Path(challenge.directory, f) for f in defaultComposeFiles] if os.path.isfile(p)]
-	if not composeFiles:
-		click.secho(
-			f"Couldn\'t find docker-compose file for challenge {challenge['name']}",
-			fg="red",
-		)
-		return
-
-	compose = composeFiles[-1]
-	if len(composeFiles) > 1:
-		click.secho(
-			f"Multiple docker-compose files found for challenge {challenge['name']}. Defaulting to {compose}",
-			fg="yellow",
-		)
-
-	with open(compose) as f:
-		doc = yaml.safe_load(f)
-
-	services = doc.get("services", {})
-	portRegex = re.compile(r".*:(.+)")
-	for key in services:
-		labels = services[key].setdefault("labels", {})
-
-		# https://docs.docker.com/compose/compose-file/compose-file-v3/#ports
-		ports = services[key].get("ports", [])
-		for i, port in enumerate(ports):
-			if (type(port) is str):
-				# 80:80 -> 80 to prevent hostPorts
-				ports[i] = re.sub(portRegex, r"\1", port)
-				
-				if domain:
-					# detect primary service by name
-					hostname = slugify(challenge['name'] if key == "app" else f"{challenge['name']}-{key}")
-
-					labels.setdefault("kompose.service.expose.tls-secret", hostname if ingressTlsSecret else "listener-tls-secret")
-					hostname = f"{hostname}.{domain}"
-				else:
-					hostname = "true"
-				labels.setdefault("kompose.service.expose", hostname)
-
-				# TODO custom property to control this on a per-service basis
-				if category == "pwn" or challenge.get("protocol", False) == "tcp":
-					labels.setdefault("kompose.service.type", "loadbalancer")
-				elif ingressClassName:
-					labels.setdefault("kompose.service.expose.ingress-class-name", ingressClassName)
-
-				if annotations:
-					for k, v in annotations:
-						labels.setdefault(k, v)
-
-			else:
-				# kompose doesn't support long syntax as of writing
+			# https://github.com/kubernetes/kompose/blob/17fbe3b4632cca0f61a7fda5f3b91034cdf5c5e4/pkg/app/app.go#L35-L43
+			defaultComposeFiles = ["compose.yaml", "compose.yml", "docker-compose.yaml", "docker-compose.yml"]
+			composeFiles = [p for p in [Path(challenge.challenge_directory, f) for f in defaultComposeFiles] if os.path.isfile(p)]
+			if len(composeFiles) > 1:
 				click.secho(
-					f"docker-compose port long syntax in challenge {challenge['name']} is not supported",
+					f"Multiple docker-compose files found for challenge {challenge['name']}. Defaulting to {compose}",
 					fg="yellow",
 				)
 
-		if registrySecret is not None:
-			labels.setdefault("kompose.image-pull-secret", registrySecret)
-		
-		# git dependency, not strictly necessary but prevents k8s redeploys if image hasn't changed
-		sha = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
-		services[key].setdefault("image", f"{registry}/{namespace}/{key}:{sha}")
+			if composeFiles:
+				deployable_challenges.append((challenge, composeFiles[-1]))
 
-	with tempfile.TemporaryDirectory() as tmp:
-		subprocess.run([
-			"kompose", "convert",
-			"-f", "-",
-			"--out", tmp,
-			"--namespace", namespace,
-			"--with-kompose-annotation=false",
-			"--build", "local",
-			"--push-image",
-			"--verbose",
-		], cwd=challenge.directory, input=yaml.dump(doc), text=True)
+		except ChallengeException as e:
+			click.secho(str(e), fg="red")
+			failed_deployments.append(challenge_key)
+			continue
 
-		# deploy namespace first so we can use it in the other manifests
-		subprocess.call(["kubectl", "apply", "-f", Path(tmp, "*-namespace.yaml")])
-		
-		# use ApplySet pruning (alpha in v1.27) to select resources, since labels aren't sufficient and standalone namespacing isn't supported
-		env = os.environ.copy()
-		env["KUBECTL_APPLYSET"] = "true"
-		subprocess.call([
-			"kubectl", "apply",
-			"-f", tmp,
-			"--prune",
-			"--namespace", namespace,
-			"--applyset", namespace
-		], env=env)
+	with click.progressbar(deployable_challenges, label="Deploying challenges") as challenges:
+		for (challenge, compose) in challenges:
+			click.echo()
 
-	# TODO set connection_info on challenge
-	# https://github.com/CTFd/ctfcli/blob/7b4a09af8414eb1f5f6da9a8422fb53b5e9cbc15/ctfcli/cli/challenges.py#L342
+			if template:
+				doc = hiyapyco.load(str(compose), template, method=hiyapyco.METHOD_MERGE)
+			else:
+				doc = hiyapyco.load(str(compose), method=hiyapyco.METHOD_MERGE)
+			
+			namespace = slugify(f"{challenge['category']}-{challenge['name']}")
+			services = doc.get("services", {})
+			portRegex = re.compile(r".*:(.+)")
 
-	# write to stdout by default eg for automatic secret creation
-	# we don't want to handle secrets, since implementations can vary a lot
-	return namespace
+			for key in services:
+				labels = services[key].setdefault("labels", {})
+
+				# https://docs.docker.com/compose/compose-file/compose-file-v3/#ports
+				ports = services[key].get("ports", [])
+				for i, port in enumerate(ports):
+					if (type(port) is str):
+						# 80:80 -> 80 to prevent hostPorts
+						ports[i] = re.sub(portRegex, r"\1", port)
+
+						# TODO custom property to control this on a per-service basis
+						if challenge['category'] == "pwn" or challenge.get("protocol", False) == "tcp":
+							labels.setdefault("kompose.service.type", "loadbalancer")
+							if key == "app":
+								challenge['connection_info'] = f"nc {domain} {ports[i]}"
+						else:
+							if domain:
+								# detect primary service by name
+								hostname = slugify(challenge['name'] if key == "app" else f"{challenge['name']}-{key}")
+								hostname = f"{hostname}.{domain}"
+
+								if key == "app":
+									challenge['connection_info'] = f"{challenge.get('protocol', 'https')}://{hostname}"
+
+							else:
+								hostname = "true"
+							labels.setdefault("kompose.service.expose", hostname)
+
+					else:
+						# kompose doesn't support long syntax as of writing
+						click.secho(
+							f"docker-compose port long syntax in challenge {challenge['name']} is not supported",
+							fg="yellow",
+						)
+				
+				# git dependency, not strictly necessary but prevents k8s redeploys if image hasn't changed
+				sha = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+				services[key].setdefault("image", f"{registry}/{namespace}/{key}:{sha}")
+
+			with tempfile.TemporaryDirectory() as tmp:
+				subprocess.run([
+					"kompose", "convert",
+					"-f", "-",
+					"--out", tmp,
+					"--namespace", namespace,
+					"--with-kompose-annotation=false",
+					"--build", "local",
+					"--push-image",
+					"--verbose",
+				], cwd=challenge.challenge_directory, input=hiyapyco.dump(doc), text=True)
+
+				# deploy namespace first so we can use it in the other manifests
+				subprocess.call(["kubectl", "apply", "-f", Path(tmp, "*-namespace.yaml")])
+				
+				# use ApplySet pruning (alpha in v1.27) to select resources, since labels aren't sufficient and standalone namespacing isn't supported
+				env = os.environ.copy()
+				env["KUBECTL_APPLYSET"] = "true"
+				subprocess.call([
+					"kubectl", "apply",
+					"-f", tmp,
+					"--prune",
+					"--namespace", namespace,
+					"--applyset", namespace
+				], env=env)
+
+			challenge.sync()
+
+	return 1
 
 def load(commands):
 	plugins = commands["plugins"]
