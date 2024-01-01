73629ede68301b8f754bd0444593e4f75d86db15
Tom Plant <tom@tplant.com.au>
2024-01-01 13:17:37 +0000

    feat: upgrade to ctfcli v1.1.0 * Use Compose YAML merging, rather than strategic. Fixes #7 * First service in Compose file is the default, rather than one named app * Parameters formatted into string to support upstream class
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- README.md
+++ README.md
@@ -4,15 +4,17 @@ A [ctfcli](https://github.com/CTFd/ctfcli) plugin for deploying CTF challenge co
 
 ## Installation
 
-1. Install the plugin: `ctf plugins install https://github.com/pl4nty/ctfcli-deploy-kubernetes`
+1. Install the plugin: `ctf plugins install https://github.com/pl4nty/ctfcli-deploy-kubernetes.git`
 2. [Install Kompose](https://kompose.io/installation/)
 3. Install `kubectl` and configure cluster access, eg [Google Kubernetes Engine docs](https://cloud.google.com/kubernetes-engine/docs/how-to/cluster-access-for-kubectl)
 4. Login to a container registry, eg `docker login ghcr.io`
 
 ## Usage
 
-1. Add a [Compose](https://www.compose-spec.io/) file like `docker-compose.yml` to your challenge folder
-2. `ctf plugins deploy_k8s --registry ghcr.io/username --challenge challengefolder`
+1. Add a [Compose](https://www.compose-spec.io/) file like `docker-compose.yml` to your challenge(s)
+2. `ctf challenge deploy --host "kubernetes://chals.example.com?registry=ghcr.io/username" --skip-login`
+3. If your Compose files already have `image` fields, the registry parameter isn't required
+4. If your `.ctf/config` file contains registry credentials, the `--skip-login` parameter isn't requried
 
 ## Pwn challenges
 
@@ -20,21 +22,21 @@ To expose pwn challenges to the internet:
 
 1. Ensure challenges have unique ports
 2. Choose a domain for challenges to be exposed on subdomains, eg `chals.example.com`
-3. `ctf plugins deploy_k8s --registry ghcr.io/username --challenge challengefolder --domain chals.example.com`
+3. `ctf challenge deploy --host "kubernetes://chals.example.com?registry=ghcr.io/username" --skip-login`
 4. Configure DNS for `chals.example.com` to point to your cloud provider's load balancer external IP address
-5. Visit your challenge at `chals.example.com:1234`
+5. Visit your challenge eg at `chals.example.com:1234`
 
 ## Web challenges
 
 To expose web challenges to the internet:
 
 1. [Install an ingress controller](https://kubernetes.io/docs/concepts/services-networking/ingress-controllers/)
-2. Set it as the default IngressClass: `kubectl annotate ingressclass your-ingress-class ingressclass.kubernetes.io/is-default-class=true`. Alternatively, set `kompose.service.expose.tls-secret` in a [template](#templating)
+2. Set it as the default IngressClass: `kubectl annotate ingressclass your-ingress-class ingressclass.kubernetes.io/is-default-class=true`. Alternatively, set `kompose.service.expose.tls-secret` in an [override](#overrides)
 3. Choose a domain for challenges to be exposed on subdomains, eg `chals.example.com`
-4. `ctf plugins deploy_k8s --registry ghcr.io/username --challenge challengefolder --domain chals.example.com`
+4. `ctf challenge deploy --host "kubernetes://chals.example.com?registry=ghcr.io/username" --skip-login`
 5. Configure DNS for `*.chals.example.com` to point to your ingress controller external IP address
-6. Visit your challenge at `http://challenge-name.chals.example.com`
-7. (Optional) Configure TLS termination at the ingress controller. If using the Ingress TLS field, create a Kubernetes secret in the challenge namespace and set `kompose.service.expose.tls-secret` in a [template](#templating). Secret creation can be automated with [reflector](https://github.com/emberstack/kubernetes-reflector) for wildcard certificates (recommended) or [cert-manager](https://cert-manager.io/).
+6. Visit your challenge at `http://challenge-name.chals.example.com`. This will be the first service in the Compose file, other services will be available with the pattern `http://challenge-name-service-name.chals.example.com`
+7. (Optional) Configure TLS termination at the ingress controller. If using the Ingress TLS field, create a Kubernetes secret in the challenge namespace and set `kompose.service.expose.tls-secret` in an [override](#override). Secret creation can be automated with [reflector](https://github.com/emberstack/kubernetes-reflector) for wildcard certificates (recommended) or [cert-manager](https://cert-manager.io/).
 
 ## Private container registries
 
@@ -47,11 +49,11 @@ kubectl create secret docker-registry ghcr --namespace=$namespace \
 --docker-password=password
 ```
 
-Then set `kompose.image-pull-secret` in a [template](#templating).
+Then set `kompose.image-pull-secret` in an [override](#override).
 
-## Templating
+## Overrides
 
-Use `--template` with a [Compose](https://www.compose-spec.io/) file to merge it into challenges. This can enable certain features. For a full list of supported `kompose` labels, see the labels section of the [Kompose docs](https://kompose.io/user-guide/).
+Use the `override` parameter with a [Compose](https://www.compose-spec.io/) file to [merge](https://docs.docker.com/compose/multiple-compose-files/merge/) it into challenges. This can enable certain features. For a full list of supported `kompose` labels, see the labels section of the [Kompose docs](https://kompose.io/user-guide/).
 
 ```yaml
 services:
--- __init__.py
+++ __init__.py
@@ -1,150 +1,205 @@
+# based on https://github.com/CTFd/ctfcli/blob/0.1.1/ctfcli/core/deployment/registry.py
+
 import logging
 import os
+from pathlib import Path
 import subprocess
 import tempfile
-import re
-from pathlib import Path
-from types import MethodType
+from urllib.parse import urlparse, parse_qs
 
 import click
 import hiyapyco
 from slugify import slugify
 
-from ctfcli.core.challenge import Challenge
 from ctfcli.core.config import Config
-from ctfcli.core.exceptions import ChallengeException
+from ctfcli.core.deployment import register_deployment_handler
+from ctfcli.core.deployment.base import DeploymentHandler, DeploymentResult
+from ctfcli.core.deployment.registry import RegistryDeploymentHandler
+from ctfcli.core.deployment.cloud import CloudDeploymentHandler
 
-log = logging.getLogger("ctfcli.cli.challenges")
+log = logging.getLogger("ctfcli.core.deployment.kubernetes")
 
-# TODO replace ingress and annotations with strategic merge patch
-def deploy_kubernetes(
-	self,
-	registry: str,
-	challenge: str = None,
-	domain: str = None,
-	template: str = None,
-	registrySecret=None, ingressTlsSecret=None
-) -> str:
-	# boilerplate from https://github.com/CTFd/ctfcli/blob/c385e70c5553dea013b1b6c99fbe353324e5f3fe/ctfcli/cli/challenges.py#L621
-	log.debug(f"deploy_k8s: (registry={registry}, challenge={challenge}, domain={domain}, template={template})")
-	
-	config = Config()
-	challenge_keys = [challenge]
 
-	if challenge is None:
-		challenge_keys = config.challenges.keys()
+class KubernetesDeploymentHandler(DeploymentHandler):
+    def __init__(self, *args, **kwargs):
+        super(KubernetesDeploymentHandler, self).__init__(*args, **kwargs)
 
-	failed_deployments = []
+        # default to tcp for pwn challenges and https for web
+        if self.challenge.get("protocol", False):
+            if self.challenge.get("category") == "pwn":
+                self.protocol = "tcp"
+            elif self.challenge.get("category") == "web":
+                self.protocol = "https"
 
-	deployable_challenges = []
-	for challenge_key in challenge_keys:
-		challenge_path = config.project_path / Path(challenge_key)
+    def deploy(self, skip_login=False, *args, **kwargs) -> DeploymentResult:
+        config = Config()
 
-		if not challenge_path.name.endswith(".yml"):
-			challenge_path = challenge_path / "challenge.yml"
+        # Check whether challenge defines image
+        # Unnecessary, but ensures compatibility with other deployment handlers
+        if not self.challenge.get("image"):
+            click.secho("Challenge does not define an image to deploy", fg="red")
+            return DeploymentResult(False)
 
-		try:
-			challenge = Challenge(challenge_path)
+        if not self.host:
+            click.secho(
+                "No host provided for the deployment. Use --host, or define host in the challenge.yml file",
+                fg="red",
+            )
+            return DeploymentResult(False)
 
-			# https://github.com/kubernetes/kompose/blob/17fbe3b4632cca0f61a7fda5f3b91034cdf5c5e4/pkg/app/app.go#L35-L43
-			defaultComposeFiles = ["compose.yaml", "compose.yml", "docker-compose.yaml", "docker-compose.yml"]
-			composeFiles = [p for p in [Path(challenge.challenge_directory, f) for f in defaultComposeFiles] if os.path.isfile(p)]
-			if len(composeFiles) > 1:
-				click.secho(
-					f"Multiple docker-compose files found for challenge {challenge['name']}. Defaulting to {compose}",
-					fg="yellow",
-				)
+        # kubernetes://public-hostname?registry=ghcr.io/pl4nty&override=compose.override.yml
+        host_url = urlparse(self.host)
+        query = parse_qs(host_url.query)
+        registry = query.get("registry", None)
+        if registry:
+            registry = registry[0]
+        override = query.get("override", None)
+        if override:
+            override = override[0]
 
-			if composeFiles:
-				deployable_challenges.append((challenge, composeFiles[-1]))
+        if skip_login:
+            click.secho(
+                "Skipping registry login because of --skip-login. Make sure you are logged in to the registry.",
+                fg="yellow",
+            )
+        else:
+            if "registry" not in config or not config["registry"]:
+                click.secho("Config does not provide a registry section.", fg="red")
+                return DeploymentResult(False)
 
-		except ChallengeException as e:
-			click.secho(str(e), fg="red")
-			failed_deployments.append(challenge_key)
-			continue
+            registry_username = config["registry"].get("username")
+            registry_password = config["registry"].get("password")
+            if not registry_username or not registry_password:
+                click.secho("Config is missing credentials for the registry.", fg="red")
+                return DeploymentResult(False)
 
-	with click.progressbar(deployable_challenges, label="Deploying challenges") as challenges:
-		for (challenge, compose) in challenges:
-			click.echo()
+            if not registry:
+                click.secho("Host is missing registry query parameter.", fg="red")
+                return DeploymentResult(False)
 
-			if template:
-				doc = hiyapyco.load(str(compose), template, method=hiyapyco.METHOD_MERGE)
-			else:
-				doc = hiyapyco.load(str(compose), method=hiyapyco.METHOD_MERGE)
-			
-			namespace = slugify(f"{challenge['category']}-{challenge['name']}")
-			services = doc.get("services", {})
-			portRegex = re.compile(r".*:(.+)")
+            login_result = RegistryDeploymentHandler._registry_login(
+                registry_username,
+                registry_password,
+                registry,
+            )
 
-			for key in services:
-				labels = services[key].setdefault("labels", {})
+            if not login_result:
+                click.secho(
+                    "Could not log in to the registry. Please check your configured credentials.",
+                    fg="red",
+                )
+                return DeploymentResult(False)
 
-				# https://docs.docker.com/compose/compose-file/compose-file-v3/#ports
-				ports = services[key].get("ports", [])
-				for i, port in enumerate(ports):
-					if (type(port) is str):
-						# 80:80 -> 80 to prevent hostPorts
-						ports[i] = re.sub(portRegex, r"\1", port)
+        # Check whether challenge has a compose file. Can't use kompose because defaults are skipped if we provide an override file
+        # https://github.com/kubernetes/kompose/blob/v1.31.2/pkg/app/app.go#L38
+        defaultComposeFiles = [
+            "compose.yaml",
+            "compose.yml",
+            "docker-compose.yaml",
+            "docker-compose.yml",
+        ]
+        composeFiles = [
+            p
+            for p in [
+                Path(self.challenge.challenge_directory, f) for f in defaultComposeFiles
+            ]
+            if os.path.isfile(p)
+        ]
+        if len(composeFiles) == 0:
+            click.secho("Challenge does not have a Compose file.", fg="red")
+            return DeploymentResult(False)
+        elif len(composeFiles) > 1:
+            click.secho(
+                f"Challenge has multiple Compose files. Defaulting to {composeFiles[0]}",
+                fg="yellow",
+            )
 
-						# TODO custom property to control this on a per-service basis
-						if challenge['category'] == "pwn" or challenge.get("protocol", False) == "tcp":
-							labels.setdefault("kompose.service.type", "loadbalancer")
-							if key == "app":
-								challenge['connection_info'] = f"nc {domain} {ports[i]}"
-						else:
-							if domain:
-								# detect primary service by name
-								hostname = slugify(challenge['name'] if key == "app" else f"{challenge['name']}-{key}")
-								hostname = f"{hostname}.{domain}"
+        doc = hiyapyco.load(str(composeFiles[0]))
+        services = doc.get("services", {})
+        connection_info = None
+        for i, key in enumerate(services):
+            # https://challenge-name-service-name.domain, or https://challenge-name.domain for the primary service
+            hostname = slugify(self.challenge.get("name"))
+            if i > 0:
+                hostname += f"-{key}"
+            hostname += f".{host_url.netloc}"
 
-								if key == "app":
-									challenge['connection_info'] = f"{challenge.get('protocol', 'https')}://{hostname}"
+            # set CTFd metadata for primary service
+            if key == 0:
+                ports = services[key].get("ports", [])
+                if len(ports) > 0:
+                    connection_info = CloudDeploymentHandler._get_connection_info(
+                        self,
+                        hostname=hostname,
+                        tcp_hostname=host_url.netloc,
+                        tcp_port=ports[0].split(":")[0],
+                    )
 
-							else:
-								hostname = "true"
-							labels.setdefault("kompose.service.expose", hostname)
+            labels = services[key].setdefault("labels", {})
+            if self.protocol == "tcp":
+                labels.setdefault("kompose.service.type", "loadbalancer")
+            else:
+                labels.setdefault("kompose.service.expose", hostname)
 
-					else:
-						# kompose doesn't support long syntax as of writing
-						click.secho(
-							f"docker-compose port long syntax in challenge {challenge['name']} is not supported",
-							fg="yellow",
-						)
-				
-				# git dependency, not strictly necessary but prevents k8s redeploys if image hasn't changed
-				sha = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
-				services[key].setdefault("image", f"{registry}/{namespace}/{key}:{sha}")
+            namespace = slugify(self.challenge.get("name"))
+            if registry:
+                services[key].setdefault("image", f"{registry}/{namespace}/{key}")
 
-			with tempfile.TemporaryDirectory() as tmp:
-				subprocess.run([
-					"kompose", "convert",
-					"-f", "-",
-					"--out", tmp,
-					"--namespace", namespace,
-					"--with-kompose-annotation=false",
-					"--build", "local",
-					"--push-image",
-					"--verbose",
-				], cwd=challenge.challenge_directory, input=hiyapyco.dump(doc), text=True)
+            # output to tmp rather than stdout so we can view logs eg Docker build
+            with tempfile.TemporaryDirectory() as tmp:
+                params = [
+                    "kompose",
+                    "convert",
+                    # we avoid self.challenge.image.build() since it doesn't support build options from compose eg multiple images
+                    "--build",
+                    "local",
+                    "--namespace",
+                    namespace,
+                    "--out",
+                    tmp,
+                    "--push-image",
+                    # "--push-image-registry" doesn't support paths eg ghcr.io/pl4nty
+                    "--with-kompose-annotation=false",
+                    "--file",
+                    "-",
+                    "--verbose",
+                ]
+                # if provided, perform a compose merge with the override file
+                # https://docs.docker.com/compose/multiple-compose-files/merge/
+                if override:
+                    params += ["--file", Path.cwd() / override]
+                subprocess.run(
+                    params,
+                    cwd=self.challenge.challenge_directory,
+                    input=hiyapyco.dump(doc),
+                    text=True,
+                )
 
-				# deploy namespace first so we can use it in the other manifests
-				subprocess.call(["kubectl", "apply", "-f", Path(tmp, "*-namespace.yaml")])
-				
-				# use ApplySet pruning (alpha in v1.27) to select resources, since labels aren't sufficient and standalone namespacing isn't supported
-				env = os.environ.copy()
-				env["KUBECTL_APPLYSET"] = "true"
-				subprocess.call([
-					"kubectl", "apply",
-					"-f", tmp,
-					"--prune",
-					"--namespace", namespace,
-					"--applyset", namespace
-				], env=env)
+                # deploy namespace first so we can use it in the other manifests
+                subprocess.call(
+                    ["kubectl", "apply", "--filename", Path(tmp, "*-namespace.yaml")]
+                )
 
-			challenge.sync()
+                # use ApplySet pruning (alpha in v1.27) to select resources, since labels aren't sufficient and standalone namespacing isn't supported
+                env = os.environ.copy()
+                env["KUBECTL_APPLYSET"] = "true"
+                subprocess.call(
+                    [
+                        "kubectl",
+                        "apply",
+                        "--filename",
+                        tmp,
+                        "--prune",
+                        "--namespace",
+                        namespace,
+                        "--applyset",
+                        namespace,
+                    ],
+                    env=env,
+                )
 
-	return 1
+        return DeploymentResult(True, connection_info=connection_info)
+
 
 def load(commands):
-	plugins = commands["plugins"]
-	plugins.deploy_k8s = MethodType(deploy_kubernetes, plugins)
+    register_deployment_handler("kubernetes", KubernetesDeploymentHandler)
